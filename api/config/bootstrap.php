60c49949cf06612c6406d37c21d87d753692c268
Brayan Venegas <b.venegas@pyansa.com.mx>
2020-05-05 14:36:38 -0600

    bootstrap.php actualizado para pyansaphp

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -12,16 +12,13 @@
  * @since         0.10.8
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 /**
  * Configure paths required to find CakePHP + general filepath
  * constants
  */
 require __DIR__ . '/paths.php';
-
 // Use composer to load the autoloader.
 require ROOT . DS . 'vendor' . DS . 'autoload.php';
-
 /**
  * Bootstrap CakePHP.
  *
@@ -32,13 +29,11 @@ require ROOT . DS . 'vendor' . DS . 'autoload.php';
  * - Setting the default application paths.
  */
 require CORE_PATH . 'config' . DS . 'bootstrap.php';
-
 // You can remove this if you are confident you have intl installed.
 // Se comenta esta linea para evitar el error al no contar con la extension intl
 /*if (!extension_loaded('intl')) {
     trigger_error('You must enable the intl extension to use CakePHP.', E_USER_ERROR);
 }*/
-
 use Cake\Cache\Cache;
 use Cake\Console\ConsoleErrorHandler;
 use Cake\Core\App;
@@ -47,19 +42,24 @@ use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Core\Plugin;
 use Cake\Database\Type;
 use Cake\Datasource\ConnectionManager;
-use Pyansa\Error\ErrorHandler;    // se cambia a Pyansa\Error\ErrorHandler
-use Cake\Log\Log;
+use Cake\Error\ErrorHandler;
+use Pyansa\Support\Facades\Log;
 use Cake\Mailer\Email;
 use Cake\Network\Request;
-use Pyansa\Routing\DispatcherFactory;   // Se cambia a Pyansa\Routing\DispatcherFactory
+use Cake\Routing\DispatcherFactory;
 use Cake\Utility\Inflector;
 use Cake\Utility\Security;
-
-// en caso que no se cargue la extension intl, se carga el plugin para compatibilidad
-if (!extension_loaded('intl')) {
-    Plugin::load('CakeIntl', ['bootstrap' => true]);
-}
-
+/**
+ * Aplicacion que servira para realizar mejoras mediante la libreria de pyansaphp/pyansaphp.
+ * Esta trabajara a la par con la aplicacion de CakePHP
+ */
+$app = new \Pyansa\Foundation\Application(realpath(__DIR__ . '/../'));
+/**
+ * Set server timezone to UTC. You can change it to another timezone of your
+ * choice but using UTC makes time calculations / conversions easier.
+ */
+// Se asigna la zona horaria de mazatlan por default
+date_default_timezone_set('America/Mazatlan');
 /**
  * Read configuration file and inject configuration into various
  * CakePHP classes.
@@ -74,12 +74,10 @@ try {
 } catch (\Exception $e) {
     die($e->getMessage() . "\n");
 }
-
 // Load an environment local configuration file.
 // You can use a file like app_local.php to provide local overrides to your
 // shared configuration.
 //Configure::load('app_local', 'default');
-
 // When debug = false the metadata cache should last
 // for a very very long time, as we don't want
 // to refresh the cache while users are doing requests.
@@ -87,40 +85,24 @@ if (!Configure::read('debug')) {
     Configure::write('Cache._cake_model_.duration', '+1 years');
     Configure::write('Cache._cake_core_.duration', '+1 years');
 }
-
-/**
- * Set server timezone to UTC. You can change it to another timezone of your
- * choice but using UTC makes time calculations / conversions easier.
- */
-// Se asigna la zona horaria de mazatlan por default
-date_default_timezone_set('America/Mazatlan');
-
 /**
  * Configure the mbstring extension to use the correct encoding.
  */
 mb_internal_encoding(Configure::read('App.encoding'));
-
 /**
  * Set the default locale. This controls how dates, number and currency is
  * formatted and sets the default language to use for translations.
  */
 ini_set('intl.default_locale', 'en_US');
-
+$isCli = PHP_SAPI === 'cli';
 /**
  * Register application error and exception handlers.
  */
-$isCli = PHP_SAPI === 'cli';
-if ($isCli) {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
-} else {
-    (new ErrorHandler(Configure::read('Error')))->register();
-}
-
+$app->register('Pyansa\Exceptions\ExceptionServiceProvider');
 // Include the CLI bootstrap overrides.
 if ($isCli) {
     require __DIR__ . '/bootstrap_cli.php';
 }
-
 /**
  * Set the full base URL.
  * This URL is used as the base of all absolute links.
@@ -132,28 +114,24 @@ if (!Configure::read('App.fullBaseUrl')) {
     if (env('HTTPS')) {
         $s = 's';
     }
-
     $httpHost = env('HTTP_HOST');
     if (isset($httpHost)) {
         Configure::write('App.fullBaseUrl', 'http' . $s . '://' . $httpHost);
     }
     unset($httpHost, $s);
 }
-
 Cache::config(Configure::consume('Cache'));
 ConnectionManager::config(Configure::consume('Datasources'));
 Email::configTransport(Configure::consume('EmailTransport'));
 Email::config(Configure::consume('Email'));
 Log::config(Configure::consume('Log'));
 Security::salt(Configure::consume('Security.salt'));
-
 /**
  * The default crypto extension in 3.0 is OpenSSL.
  * If you are migrating from 2.x uncomment this code to
  * use a more compatible Mcrypt based implementation
  */
 // Security::engine(new \Cake\Utility\Crypto\Mcrypt());
-
 /**
  * Setup detectors for mobile and tablet.
  */
@@ -165,7 +143,6 @@ Request::addDetector('tablet', function ($request) {
     $detector = new \Detection\MobileDetect();
     return $detector->isTablet();
 });
-
 /**
  * Custom Inflector rules, can be set to correctly pluralize or singularize
  * table, model, controller names or whatever other string is passed to the
@@ -176,7 +153,6 @@ Request::addDetector('tablet', function ($request) {
  * Inflector::rules('uninflected', ['dontinflectme']);
  * Inflector::rules('transliteration', ['/å/' => 'aa']);
  */
-
 /**
  * Plugins need to be loaded manually, you can either load them one by one or all of them in a single call
  * Uncomment one of the lines below, as you need. make sure you read the documentation on Plugin to use more
@@ -186,28 +162,31 @@ Request::addDetector('tablet', function ($request) {
  * Plugin::load('Migrations'); //Loads a single plugin named Migrations
  *
  */
-
 Plugin::load('Migrations');
-
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
-
 /**
  * Connect middleware/dispatcher filters.
  */
 DispatcherFactory::add('Asset');
 DispatcherFactory::add('Routing');
 DispatcherFactory::add('ControllerFactory');
-
-// se agrega esta linea para registrar filtros adicionales
-DispatcherFactory::registerFilters();
-
 /**
  * Enable default locale format parsing.
  * This is needed for matching the auto-localized string output of Time() class when parsing dates.
  */
 Type::build('date')->useLocaleParser();
 Type::build('datetime')->useLocaleParser();
+/**
+ * Registra los service providers necesarios para la aplicacion, decidiendo si es console o HTTP
+ */
+if (PHP_SAPI === 'cli') {
+    $app->register('Pyansa\Console\ConsoleServiceProvider');
+} else {
+    $app->register('Pyansa\Http\HttpServiceProvider');
+}
+// Bootea la aplicacion
+$app->boot();
